70e225e41090222a3916370e2f427b20f893b475
Asbudi Anugrah <70067775+marchNum13@users.noreply.github.com>
2025-07-30 03:02:08 +0800

    a

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss' // <-- Import tailwindcss
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // Tambahkan tailwindcss() ke dalam array plugins
+  plugins: [react(), tailwindcss()], 
 })
